8a7c0e330ce6487a7fbbcb56ef0e8137afe01891
Juliano Firme <julianofirme23@gmail.com>
2025-02-06 19:20:39 -0300

    hidden suggestions

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -27,7 +27,7 @@ export function EmptyScreen({
   className?: string
 }) {
   return (
-    <div className={`mx-auto w-full transition-all ${className}`}>
+    <div className={`mx-auto w-full transition-all hidden ${className}`}>
       <div className="bg-background p-2">
         <div className="mt-2 flex flex-col items-start space-y-2 mb-4">
           {exampleMessages.map((message, index) => (
